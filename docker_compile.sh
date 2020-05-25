93eb8622006daa78fee9c5e98fce4d83aed6174a
Adam Rousell <adam.rousell@uni-heidelberg.de>
2020-05-25 14:43:06 +0000

    Added parameters to script for osm and elevation filepaths

--- docker_compile.sh
+++ docker_compile.sh
@@ -1,4 +1,17 @@
-#!/usr/bin/env sh
+#!/bin/bash
+
+while getopts o:e: option
+do
+case "${option}"
+in
+o) OSM_PATH=${OPTARG};;
+e) ELEVATION_PATH=${OPTARG};;
+esac
+done
+
+if [ -z ${OSM_PATH+x} ]; then OSM_PATH=/opt/ors/osm; fi
+if [ -z ${ELEVATION_PATH+x} ]; then ELEVATION_PATH=/opt/ors/elevation; fi
+
 docker build -t ors-preprocessor -f Dockerfile .
-docker run -it -v /opt/ors/elevation/preprocessor:/elevation -v /opt/ors/osm/preprocessor:/osm --name ors-preprocessor ors-preprocessor -m -o /osm/planet-latest.osm.pbf 
+docker run -it -v $ELEVATION_PATH:/elevation -v $OSM_PATH:/osm --name ors-preprocessor ors-preprocessor -m -o /osm/planet-latest.osm.pbf 
 docker rm ors-preprocessor
