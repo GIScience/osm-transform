3571d10079303a540745bf7adb3b2d5cfd5d2e25
Adam Rousell <adam.rousell@uni-heidelberg.de>
2020-05-25 14:32:31 +0000

    Added missing parameter to docker run

--- docker_compile.sh
+++ docker_compile.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 docker build -t ors-preprocessor -f Dockerfile .
-docker run -i -v /opt/ors/elevation/preprocessor:/elevation -v /opt/ors/osm/preprocessor:/osm --name ors-preprocessor ors-preprocessor -m -o /osm/planet-latest.osm.pbf 
+docker run -it -v /opt/ors/elevation/preprocessor:/elevation -v /opt/ors/osm/preprocessor:/osm --name ors-preprocessor ors-preprocessor -m -o /osm/planet-latest.osm.pbf 
 docker rm ors-preprocessor
