1c763dd35789eb8365d2555321f29e97c6377351
Adam Rousell <adam.rousell@uni-heidelberg.de>
2020-05-25 13:34:39 +0000

    Update docker_compile.sh to use the newer docker container

--- docker_compile.sh
+++ docker_compile.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 docker build -t ors-preprocessor -f Dockerfile .
-docker run -i -v $(pwd):/src ors-preprocessor
+docker run -i -v /opt/ors/elevation/preprocessor:/elevation -v /opt/ors/osm/preprocessor:/osm ors-preprocessor mo /osm/planet-latest.osm.pbf 
 docker container prune -f
 docker image rm ors-preprocessor:latest
