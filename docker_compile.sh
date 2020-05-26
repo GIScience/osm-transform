ee6feb90eb247b9e8f6ee886d8e60226f48b1b86
Adam Rousell <adam.rousell@heigit.org>
2020-05-26 15:21:01 +0200

    Added parameter to compile script for osm filename

--- docker_compile.sh
+++ docker_compile.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-while getopts o:e: option
+while getopts o:e:f: option
 do
 case "${option}"
 in
 o) OSM_PATH=${OPTARG};;
 e) ELEVATION_PATH=${OPTARG};;
+f) OSM_FILE=${OPTARG};;
 esac
 done
 
 if [ -z ${OSM_PATH+x} ]; then OSM_PATH=/opt/ors/osm; fi
 if [ -z ${ELEVATION_PATH+x} ]; then ELEVATION_PATH=/opt/ors/elevation; fi
+if [ -z ${OSM_FILE+x} ]; then OSM_FILE=planet-latest.osm.pbf; fi
 
 docker build -t ors-preprocessor -f Dockerfile .
-docker run -it -v $ELEVATION_PATH:/elevation -v $OSM_PATH:/osm --name ors-preprocessor ors-preprocessor -m -o /osm/planet-latest.osm.pbf 
+docker run -it -v $ELEVATION_PATH:/elevation -v $OSM_PATH:/osm --name ors-preprocessor ors-preprocessor -m -o /osm/$OSM_FILE 
 docker rm ors-preprocessor
