d57faac2f6ecce0438dc61f7d0ef5a79c9e3b761
Adam Rousell <adam.rousell@uni-heidelberg.de>
2020-05-27 08:35:09 +0000

    move to the directory for the Dockerfile before trying to build

--- docker_compile.sh
+++ docker_compile.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-while getopts o:e:f: option
+while getopts o:e:f:p: option
 do
 case "${option}"
 in
 o) OSM_PATH=${OPTARG};;
 e) ELEVATION_PATH=${OPTARG};;
 f) OSM_FILE=${OPTARG};;
+p) ROOT_PATH=${OPTARG};;
 esac
 done
 
 if [ -z ${OSM_PATH+x} ]; then OSM_PATH=/opt/ors/osm; fi
 if [ -z ${ELEVATION_PATH+x} ]; then ELEVATION_PATH=/opt/ors/elevation; fi
 if [ -z ${OSM_FILE+x} ]; then OSM_FILE=planet-latest.osm.pbf; fi
+if [ -z ${ROOT_PATH+x} ]; then ROOT_PATH=/opt/ors/ors-preprocessor; fi
 
+cd $ROOT_PATH
 docker build -t ors-preprocessor -f Dockerfile .
 docker run -it -v $ELEVATION_PATH:/elevation -v $OSM_PATH:/osm --name ors-preprocessor ors-preprocessor -m -o /osm/$OSM_FILE 
 docker rm ors-preprocessor
