fbe35abbdcf599ea0932e7137e21ee999068aabf
Amandus Butzer <amandus.butzer@heigit.org>
2023-05-04 17:17:30 +0200

    test(cli): add basic srtm-download tests
    - checks for single tile file existence
    - checks stdout log output for strings

--- cli/tests/test_main.py
+++ cli/tests/test_main.py
@@ -1,6 +1,9 @@
+import os
+
 from typer.testing import CliRunner
 
 from cli.osm_transform import __app_name__, __version__, main
+from osm_transform.srtm_data import STORAGE_FOLDER
 
 runner = CliRunner()
 
@@ -14,3 +17,26 @@ def test_version() -> None:
 def test_foo():
     result = runner.invoke(main.app, ["--logging=debug", "foo"])
     assert result.exit_code == 0
+
+
+def test_srtm_download():
+    srtm_file_path = STORAGE_FOLDER / 'srtm_01_02.tif'
+
+    if srtm_file_path.exists():
+        os.remove(srtm_file_path)
+
+    assert srtm_file_path.exists() is False
+
+    result = runner.invoke(main.app, ["--logging=debug", "srtm-download", "--x=1", "--y=2"])
+    assert result.exit_code == 0
+    assert "INFO - Files downloaded 1 of 1 (0 files already present)" in result.stdout.split('\n')[-2]
+    assert srtm_file_path.exists() is True
+
+    result = runner.invoke(main.app, ["--logging=debug", "srtm-download", "--x=1", "--y=2"])
+    assert result.exit_code == 0
+    assert "INFO - Files downloaded 0 of 1 (1 files already present)" in result.stdout.split('\n')[-2]
+
+    result = runner.invoke(main.app, ["--logging=debug", "srtm-download", "--x=1", "--y=1"])
+    assert result.exit_code == 0
+    assert "is not a valid tile." in result.stdout.split('\n')[-3]
+    assert "INFO - Files downloaded 0 of 1 (0 files already present)" in result.stdout.split('\n')[-2]
