6a6c02a6e0243e9cdbacc800f8689fa4fddb44fd
Rafael Troilo <rafael.troilo@heigit.org>
2024-03-09 11:37:17 +0000

    feat: improve tests

--- test/test_location_area.cpp
+++ test/test_location_area.cpp
@@ -2,16 +2,32 @@
 
 #include "../location_area_service.h"
 
-BOOST_AUTO_TEST_CASE(test_location_area_service)
+BOOST_AUTO_TEST_SUITE( test_locacion_area )
+BOOST_AUTO_TEST_CASE( test_location_area_service )
 {
     std::string geo_type("wkt");
     std::string prefix("mapping_");
     LocationAreaService location_area_service(true, 0, 2, geo_type, true, prefix);
     location_area_service.load("test/mapping_test.csv");
 
-    osmium::Location location = osmium::Location(6.306152343750001, 50.05713877598692);
-    std::vector<std::string> areas = location_area_service.get_area(location);
+    {
+        const auto areas = location_area_service.get_area(osmium::Location(6.306152343750001, 50.05713877598692));
+        BOOST_CHECK_EQUAL(areas.size(), 1);
+        BOOST_CHECK_EQUAL(areas[0], "DEU");
+    }
 
-    BOOST_TEST(areas.size() == 1);
-    BOOST_TEST(areas[0] == "DEU");
+    {
+        const auto areas = location_area_service.get_area(osmium::Location(6.0900938, 50.7225850));
+        BOOST_CHECK_EQUAL(areas.size(), 1);
+        BOOST_CHECK_EQUAL(areas[0], "DEU");
+    }
+
+    {
+        const auto areas = location_area_service.get_area(osmium::Location(6.0902180,  50.7220057));
+        BOOST_CHECK_EQUAL(areas.size(), 1);
+        BOOST_CHECK_EQUAL(areas[0], "BEL");
+    }
+
+
 }
+BOOST_AUTO_TEST_SUITE_END()
--- test/test_rewrite_handler.cpp
+++ test/test_rewrite_handler.cpp
@@ -9,7 +9,7 @@
 
 BOOST_AUTO_TEST_SUITE( test_rewrite_pass )
 
-BOOST_AUTO_TEST_CASE (pass) {
+BOOST_AUTO_TEST_CASE (rewrite_full_pass) {
     auto debug_mode = false;
     std::string geo_type{"wkt"};
     std::string prefix{"mapping_"};
@@ -23,47 +23,120 @@ BOOST_AUTO_TEST_CASE (pass) {
     location_area_service.load("test/mapping_test.csv");
 
     osmium::nwr_array<osmium::index::IdSetDense<osmium::unsigned_object_id_type>> valid_ids;
-    valid_ids.nodes().set(101);
-    valid_ids.nodes().set(102);
-    valid_ids.nodes().set(301);
-    valid_ids.nodes().set(302);
-    valid_ids.ways().set(10);
-    valid_ids.ways().set(30);
-
     osmium::nwr_array<osmium::index::IdSetSmall<osmium::unsigned_object_id_type>> no_elevation;
-    no_elevation.nodes().set(101);
-    no_elevation.nodes().set(102);
-
     boost::regex remove_tag_regex("(.*:)?source(:.*)?|(.*:)?note(:.*)?|url|created_by|fixme|wikipedia", boost::regex::icase);
-
     auto interpolate = false;
     auto interpolate_threshold = 0.5;
     RewriteHandler handler(1000000000, location_index, location_elevation_service, location_area_service, remove_tag_regex, valid_ids, no_elevation, interpolate, interpolate_threshold);
 
     osmium::memory::Buffer input{1 << 10, osmium::memory::Buffer::auto_grow::yes};
-    add_node(input, 101, {}, 0.0, 0.0);
-    add_node(input, 102, {}, 0.0, 0.0);
+
+    add_node(input, 101, {{"fixme","name"}, {"FIXME","yes"}, {"FixME", "check"}}, 0.0, 0.0);
+    valid_ids.nodes().set(101);
+    no_elevation.nodes().set(101);
+    add_node(input, 102, {{"ors:source", "transform"}, {"note:check", "yes"}}, 0.0, 0.0);
+    valid_ids.nodes().set(102);
+    no_elevation.nodes().set(102);
+
     add_node(input, 201, {}, 0.0, 0.0);
     add_node(input, 202, {}, 0.0, 0.0);
-    add_node(input, 301, {}, 0.0, 0.0);
-    add_node(input, 302, {}, 0.0, 0.0);
-    input.commit();
 
+    add_node(input, 301, {{"highway","crossing"}}, 0.0, 0.0);
+    valid_ids.nodes().set(301);
+    add_node(input, 302, {}, 0.0, 0.0);
+    valid_ids.nodes().set(302);
+    add_node(input, 91142609, {}, 6.0902180, 50.7220057);
+    valid_ids.nodes().set(91142609);
+    add_node(input, 270418052, {}, 8.6761206, 49.4181246);
+    valid_ids.nodes().set(270418052);
+    add_node(input, 278110816, {}, 6.0900938, 50.7225850);
+    valid_ids.nodes().set(278110816);
+    add_node(input, 6772131501, {},8.6760972, 49.4181246);
+    valid_ids.nodes().set(6772131501);
+
+    add_way(input, 10, {{"highway","yes"}}, { 101, 102 });
+    valid_ids.ways().set(10);
+    add_way(input, 30, {{"highway","yes"}}, { 301, 302 });
+    valid_ids.ways().set(30);
+
+    add_way(input,721933838, {{"highway", "primary"}, {"name", "Berliner Straße"}}, { 270418052, 721933838 });
+    valid_ids.ways().set(721933838);
+    input.commit();
 
     osmium::memory::Buffer output_nodes{1 << 10, osmium::memory::Buffer::auto_grow::yes};
     osmium::memory::Buffer output_ways{1 << 10, osmium::memory::Buffer::auto_grow::yes};
     handler.set_buffers(&output_ways, &output_nodes);
     osmium::apply(input, handler);
+    
+    {
+        auto nodes = output_nodes.select<osmium::Node>();
+        BOOST_CHECK_EQUAL(nodes.size(), 8);
+        auto item = nodes.begin();
+        {
+            const auto& node = (*item++);
+            BOOST_CHECK_EQUAL(node.id(), 101);
+            BOOST_CHECK(node.tags().empty());
+        }
+        {
+            const auto &node = (*item++);
+            BOOST_CHECK_EQUAL(node.id(), 102);
+            BOOST_CHECK(node.tags().empty());
+        }
+        {
+            const auto &node = (*item++);
+            BOOST_CHECK_EQUAL(node.id(), 301);
+            BOOST_CHECK_EQUAL(node.tags().size(), 1);
+            BOOST_CHECK(node.tags().has_tag("highway", "crossing"));
+        }
+        {
+            const auto &node = (*item++);
+            BOOST_CHECK_EQUAL(node.id(), 302);
+        }
+        {
+            const auto &node = (*item++);
+            BOOST_CHECK_EQUAL(node.id(), 91142609);
+            BOOST_CHECK_EQUAL(node.tags().size(), 1);
+            BOOST_CHECK_EQUAL(node.tags().get_value_by_key("country",""), "BEL");
+        }
+        {
+            const auto &node = (*item++);
+            BOOST_CHECK_EQUAL(node.id(), 270418052);
+            BOOST_CHECK_EQUAL(node.tags().size(), 1);
+            BOOST_CHECK_EQUAL(node.tags().get_value_by_key("country",""), "DEU");
+        }
+        {
+            const auto &node = (*item++);
+            BOOST_CHECK_EQUAL(node.id(), 278110816);
+            BOOST_CHECK_EQUAL(node.tags().size(), 1);
+            BOOST_CHECK_EQUAL(node.tags().get_value_by_key("country",""), "DEU");
+        }
+        {
+            const auto &node = (*item++);
+            BOOST_CHECK_EQUAL(node.id(), 6772131501);
+            BOOST_CHECK_EQUAL(node.tags().size(), 1);
+            BOOST_CHECK_EQUAL(node.tags().get_value_by_key("country",""), "DEU");
+        }
+    }
 
-    BOOST_CHECK_EQUAL(4, output_nodes.select<osmium::Node>().size());
-
-    auto iter = output_nodes.select<osmium::Node>();
-    auto item = iter.begin();
-    BOOST_CHECK_EQUAL(101, (*item++).id());
-    // check elevation, check country, check tags
-    BOOST_CHECK_EQUAL(102, (*item++).id());
-    BOOST_CHECK_EQUAL(301, (*item++).id());
-    BOOST_CHECK_EQUAL(302, (*item++).id());
+    {
+        auto ways = output_ways.select<osmium::Way>();
+        BOOST_CHECK_EQUAL(ways.size(), 3);
+        auto item = ways.begin();
+        {
+            const auto& way = (*item++);
+            BOOST_CHECK_EQUAL(way.id(), 10);
+            BOOST_CHECK_EQUAL(way.tags().size(), 1);
+        }
+        {
+            const auto& way = (*item++);
+            BOOST_CHECK_EQUAL(way.id(), 30);
+        }
+        {
+            const auto& way = (*item++);
+            BOOST_CHECK_EQUAL(way.id(), 721933838);
+            BOOST_CHECK_EQUAL(way.tags().size(), 2);
+        }
+    }
 
 }
 
