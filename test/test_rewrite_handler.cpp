538f85e58afe744e6473a3c3a678f1fe47cb35c9
Rafael Troilo <rafael.troilo@heigit.org>
2024-03-09 12:39:38 +0000

    feat: improve tests

--- test/test_rewrite_handler.cpp
+++ test/test_rewrite_handler.cpp
@@ -8,6 +8,155 @@
 #include "test_utils.h"
 
 BOOST_AUTO_TEST_SUITE( test_rewrite_pass )
+BOOST_AUTO_TEST_CASE (interpolation_0_5) {
+    auto debug_mode = false;
+    std::string geo_type{"wkt"};
+    std::string prefix{"mapping_"};
+    std::string index_type{"flex_mem"};
+
+    const auto &map_factory = osmium::index::MapFactory<osmium::unsigned_object_id_type, osmium::Location>::instance();
+    auto location_index = map_factory.create_map(index_type);
+
+    LocationElevationService location_elevation_service(1 << 20, debug_mode);
+    location_elevation_service.load({"files/limburg_an_der_lahn.tif"});
+
+    LocationAreaService location_area_service(debug_mode, 0, 2, geo_type, true, prefix);
+    location_area_service.load("test/mapping_test.csv");
+
+    osmium::nwr_array<osmium::index::IdSetDense<osmium::unsigned_object_id_type>> valid_ids;
+    osmium::nwr_array<osmium::index::IdSetSmall<osmium::unsigned_object_id_type>> no_elevation;
+    boost::regex remove_tag_regex("(.*:)?source(:.*)?|(.*:)?note(:.*)?|url|created_by|fixme|wikipedia", boost::regex::icase);
+    auto interpolate = true;
+    auto interpolate_threshold = 0.5;
+
+    osmium::memory::Buffer input{1 << 10, osmium::memory::Buffer::auto_grow::yes};
+
+    add_node(input, 101, {}, 8.0515393, 50.3873984);
+    add_node(input, 102, {}, 8.0505023, 50.3868868);
+    valid_ids.nodes().set(101);
+    valid_ids.nodes().set(102);
+    add_way(input, 10, {}, {101, 102});
+    valid_ids.ways().set(10);
+
+    input.commit();
+
+    RewriteHandler handler(1000000000, location_index, location_elevation_service, location_area_service, remove_tag_regex, valid_ids, no_elevation, interpolate, interpolate_threshold);
+    osmium::memory::Buffer output_nodes{1 << 10, osmium::memory::Buffer::auto_grow::yes};
+    osmium::memory::Buffer output_ways{1 << 10, osmium::memory::Buffer::auto_grow::yes};
+    handler.set_buffers(&output_ways, &output_nodes);
+    osmium::apply(input, handler);
+
+    {
+        auto nodes = output_nodes.select<osmium::Node>();
+        BOOST_CHECK_EQUAL(nodes.size(), 11);
+        //        auto item = nodes.begin();
+        //        {
+        //            const auto& node = (*item++);
+        //            BOOST_CHECK_EQUAL(node.id(), 101);
+        //            BOOST_CHECK(node.tags().empty());
+        //        }
+    }
+    {
+        auto ways = output_ways.select<osmium::Way>();
+        BOOST_CHECK_EQUAL(ways.size(), 1);
+    }
+}
+
+BOOST_AUTO_TEST_CASE (interpolation_1_0) {
+    auto debug_mode = false;
+    std::string geo_type{"wkt"};
+    std::string prefix{"mapping_"};
+    std::string index_type{"flex_mem"};
+
+    const auto &map_factory = osmium::index::MapFactory<osmium::unsigned_object_id_type, osmium::Location>::instance();
+    auto location_index = map_factory.create_map(index_type);
+
+    LocationElevationService location_elevation_service(1 << 20, debug_mode);
+    location_elevation_service.load({"files/limburg_an_der_lahn.tif"});
+
+    LocationAreaService location_area_service(debug_mode, 0, 2, geo_type, true, prefix);
+    location_area_service.load("test/mapping_test.csv");
+
+    osmium::nwr_array<osmium::index::IdSetDense<osmium::unsigned_object_id_type>> valid_ids;
+    osmium::nwr_array<osmium::index::IdSetSmall<osmium::unsigned_object_id_type>> no_elevation;
+    boost::regex remove_tag_regex("(.*:)?source(:.*)?|(.*:)?note(:.*)?|url|created_by|fixme|wikipedia", boost::regex::icase);
+    auto interpolate = true;
+    auto interpolate_threshold = 1.0;
+
+    osmium::memory::Buffer input{1 << 10, osmium::memory::Buffer::auto_grow::yes};
+
+    add_node(input, 101, {}, 8.0515393, 50.3873984);
+    add_node(input, 102, {}, 8.0505023, 50.3868868);
+    valid_ids.nodes().set(101);
+    valid_ids.nodes().set(102);
+    add_way(input, 10, {}, {101, 102});
+    valid_ids.ways().set(10);
+
+    input.commit();
+
+    RewriteHandler handler(1000000000, location_index, location_elevation_service, location_area_service, remove_tag_regex, valid_ids, no_elevation, interpolate, interpolate_threshold);
+    osmium::memory::Buffer output_nodes{1 << 10, osmium::memory::Buffer::auto_grow::yes};
+    osmium::memory::Buffer output_ways{1 << 10, osmium::memory::Buffer::auto_grow::yes};
+    handler.set_buffers(&output_ways, &output_nodes);
+    osmium::apply(input, handler);
+
+    {
+        auto nodes = output_nodes.select<osmium::Node>();
+        BOOST_CHECK_EQUAL(nodes.size(), 6);
+    }
+    {
+        auto ways = output_ways.select<osmium::Way>();
+        BOOST_CHECK_EQUAL(ways.size(), 1);
+    }
+}
+
+BOOST_AUTO_TEST_CASE (interpolation_10_0) {
+    auto debug_mode = false;
+    std::string geo_type{"wkt"};
+    std::string prefix{"mapping_"};
+    std::string index_type{"flex_mem"};
+
+    const auto &map_factory = osmium::index::MapFactory<osmium::unsigned_object_id_type, osmium::Location>::instance();
+    auto location_index = map_factory.create_map(index_type);
+
+    LocationElevationService location_elevation_service(1 << 20, debug_mode);
+    location_elevation_service.load({"files/limburg_an_der_lahn.tif"});
+
+    LocationAreaService location_area_service(debug_mode, 0, 2, geo_type, true, prefix);
+    location_area_service.load("test/mapping_test.csv");
+
+    osmium::nwr_array<osmium::index::IdSetDense<osmium::unsigned_object_id_type>> valid_ids;
+    osmium::nwr_array<osmium::index::IdSetSmall<osmium::unsigned_object_id_type>> no_elevation;
+    boost::regex remove_tag_regex("(.*:)?source(:.*)?|(.*:)?note(:.*)?|url|created_by|fixme|wikipedia", boost::regex::icase);
+    auto interpolate = true;
+    auto interpolate_threshold = 10.0;
+
+    osmium::memory::Buffer input{1 << 10, osmium::memory::Buffer::auto_grow::yes};
+
+    add_node(input, 101, {}, 8.0515393, 50.3873984);
+    add_node(input, 102, {}, 8.0505023, 50.3868868);
+    valid_ids.nodes().set(101);
+    valid_ids.nodes().set(102);
+    add_way(input, 10, {}, {101, 102});
+    valid_ids.ways().set(10);
+
+    input.commit();
+
+    RewriteHandler handler(1000000000, location_index, location_elevation_service, location_area_service, remove_tag_regex, valid_ids, no_elevation, interpolate, interpolate_threshold);
+    osmium::memory::Buffer output_nodes{1 << 10, osmium::memory::Buffer::auto_grow::yes};
+    osmium::memory::Buffer output_ways{1 << 10, osmium::memory::Buffer::auto_grow::yes};
+    handler.set_buffers(&output_ways, &output_nodes);
+    osmium::apply(input, handler);
+
+    {
+        auto nodes = output_nodes.select<osmium::Node>();
+        BOOST_CHECK_EQUAL(nodes.size(), 2);
+    }
+    {
+        auto ways = output_ways.select<osmium::Way>();
+        BOOST_CHECK_EQUAL(ways.size(), 1);
+    }
+}
 
 BOOST_AUTO_TEST_CASE (rewrite_full_pass) {
     auto debug_mode = false;
@@ -67,7 +216,7 @@ BOOST_AUTO_TEST_CASE (rewrite_full_pass) {
     osmium::memory::Buffer output_ways{1 << 10, osmium::memory::Buffer::auto_grow::yes};
     handler.set_buffers(&output_ways, &output_nodes);
     osmium::apply(input, handler);
-    
+
     {
         auto nodes = output_nodes.select<osmium::Node>();
         BOOST_CHECK_EQUAL(nodes.size(), 8);
