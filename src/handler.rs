06a3f2ef522a601d730433e69a7c5321fe2f405a
Jochen Haeussler <jochen.haeussler@heigit.org>
2024-05-02 19:34:56 +0200

    feat: change new impl to use Node

--- src/handler.rs
+++ src/handler.rs
@@ -1,3 +1,4 @@
+use osm_io::osm::model::node::Node;
 use regex::Regex;
 
 struct HandlerResult {
@@ -13,7 +14,7 @@ impl HandlerResult {
 
 
 trait Handler {
-    fn handle(&mut self, value: String);
+    fn handle_node(&mut self, node: &Node);
     fn get_result(&mut self, result: HandlerResult) -> HandlerResult;
 }
 
@@ -26,8 +27,8 @@ impl Terminator {
 }
 
 impl Handler for Terminator {
-    fn handle(&mut self, value: String) {
-        println!("terminator received {value}")
+    fn handle_node(&mut self, node: &Node) {
+        println!("terminator received {:?}", node)
     }
     fn get_result(&mut self, result: HandlerResult) -> HandlerResult {
         result
@@ -47,9 +48,9 @@ struct NodesCounter {
 }
 
 impl Handler for NodesCounter {
-    fn handle(&mut self, value: String) {
+    fn handle_node(&mut self, node: &Node) {
         self.count += 1;
-        self.next.handle(value)
+        self.next.handle_node(node)
     }
     fn get_result(&mut self, mut result: HandlerResult) -> HandlerResult {
         match self.count_type {
@@ -65,7 +66,8 @@ enum FilterType {
     AcceptMatching,
     RemoveMatching,
 }
-struct NodesFilter {
+struct NodesFilterForTagValueMatch {
+    pub tag: String,
     pub regex: Regex,
     pub filter_type: FilterType,
     pub next: Box<dyn Handler + 'static>,
@@ -77,7 +79,7 @@ enum HandlerDef {
     NodesCounter(NodesCounterDef),
 }
 #[derive(Debug)]
-struct NodesFilterDef{ regex: Regex, filter_type: FilterType }
+struct NodesFilterDef{tag: String, regex: Regex, filter_type: FilterType }
 #[derive(Debug)]
 struct NodesCounterDef{ count_type: CountType }
 
@@ -88,7 +90,7 @@ fn as_chain(mut defs: Vec<HandlerDef>) -> Box<dyn Handler> {
         match hander_def {
             HandlerDef::NodesFilter(def) => {
                 // println!("regex: {:?}, filter_type: {:?}", &def.regex, &def.filter_type);
-                previous = Box::new(NodesFilter{filter_type: def.filter_type, regex: def.regex, next: previous });
+                previous = Box::new(NodesFilterForTagValueMatch {tag: def.tag, filter_type: def.filter_type, regex: def.regex, next: previous });
             }
             HandlerDef::NodesCounter(def) => {
                 // println!("count_type: {:?}", &def.count_type);
@@ -99,17 +101,26 @@ fn as_chain(mut defs: Vec<HandlerDef>) -> Box<dyn Handler> {
     previous
 }
 
-impl Handler for NodesFilter {
-    fn handle(&mut self, value: String) {
+impl Handler for NodesFilterForTagValueMatch {
+    fn handle_node(&mut self, node: &Node) {
         match self.filter_type {
             FilterType::AcceptMatching => {
-                if self.regex.is_match(&value) {
-                    self.next.handle(value)
+                for tag in node.tags() {
+                    if self.tag.eq(tag.k()) && self.regex.is_match(tag.v()) {
+                        self.next.handle_node(node)
+                    }
                 }
             }
             FilterType::RemoveMatching => {
-                if !self.regex.is_match(&value) {
-                    self.next.handle(value)
+                let mut found_match = false;
+                for tag in node.tags() {
+                    if self.tag.eq(tag.k()) && self.regex.is_match(tag.v()) {
+                        found_match = true;
+                        break;
+                    }
+                }
+                if !found_match {
+                    self.next.handle_node(node)
                 }
             }
         }
@@ -123,37 +134,35 @@ impl Handler for NodesFilter {
 
 #[cfg(test)]
 mod tests {
+    use osm_io::osm::model::coordinate::Coordinate;
+    use osm_io::osm::model::node::Node;
+    use osm_io::osm::model::tag::Tag;
     use regex::Regex;
-    use crate::handler::{as_chain, CountType, FilterType, Handler, HandlerDef, HandlerResult, NodesCounter, NodesCounterDef, NodesFilter, NodesFilterDef, Terminator};
+    use crate::handler::{as_chain, CountType, FilterType, Handler, HandlerDef, HandlerResult, NodesCounter, NodesCounterDef, NodesFilterForTagValueMatch, NodesFilterDef, Terminator};
 
     #[test]
-    fn test_as_chain() {
+    fn test_handle_nodes_with_as_chain() {
         let handlers = vec![
             HandlerDef::NodesCounter(NodesCounterDef{count_type: CountType::ALL}),
-            HandlerDef::NodesFilter(NodesFilterDef{regex: Regex::new(".*p.*").unwrap(), filter_type: FilterType::AcceptMatching }),
-            HandlerDef::NodesFilter(NodesFilterDef{regex: Regex::new(".*z.*").unwrap(), filter_type: FilterType::RemoveMatching }),
+            HandlerDef::NodesFilter(NodesFilterDef{tag: "bla".to_string(), regex: Regex::new(".*p.*").unwrap(), filter_type: FilterType::AcceptMatching }),
+            HandlerDef::NodesFilter(NodesFilterDef{tag: "bla".to_string(), regex: Regex::new(".*z.*").unwrap(), filter_type: FilterType::RemoveMatching }),
             HandlerDef::NodesCounter(NodesCounterDef{count_type: CountType::ACCEPTED}),
         ];
         let mut handler = as_chain(handlers);
-
-        handler.handle("kasper".to_string());
-        handler.handle("seppl".to_string());
-        handler.handle("hotzenplotz".to_string());
-        handler.handle("großmutter".to_string());
-        let result = handler.get_result(HandlerResult::new());
-        assert_eq!(result.count_all_nodes, 4);
-        assert_eq!(result.count_accepted_nodes, 2);
+        handle_test_nodes_and_verify_result(&mut *handler);
     }
 
     #[test]
-    fn test_get_field() {
+    fn test_handle_nodes_with_manually_chanied_handlesr() {
         let mut handler = NodesCounter {
             count: 0,
             count_type: CountType::ALL,
-            next: Box::new(NodesFilter {
+            next: Box::new(NodesFilterForTagValueMatch {
+                tag: "bla".to_string(),
                 filter_type: FilterType::AcceptMatching,
                 regex: Regex::new(".*p.*").unwrap(),
-                next: Box::new(NodesFilter {
+                next: Box::new(NodesFilterForTagValueMatch {
+                    tag: "bla".to_string(),
                     filter_type: FilterType::RemoveMatching,
                     regex: Regex::new(".*z.*").unwrap(),
                     next: Box::new(NodesCounter {
@@ -164,13 +173,17 @@ mod tests {
                 }),
             }),
         };
+        handle_test_nodes_and_verify_result(&mut handler);
+    }
 
-        handler.handle("kasper".to_string());
-        handler.handle("seppl".to_string());
-        handler.handle("hotzenplotz".to_string());
-        handler.handle("großmutter".to_string());
+    fn handle_test_nodes_and_verify_result(handler: &mut dyn Handler) {
+        handler.handle_node(&Node::new(1, 1, Coordinate::new(1.0f64, 1.0f64), 1, 1, 1, "a".to_string(), true, vec![Tag::new("bla".to_string(), "seppl".to_string())]));
+        handler.handle_node(&Node::new(2, 1, Coordinate::new(1.0f64, 1.0f64), 1, 1, 1, "a".to_string(), true, vec![Tag::new("bla".to_string(), "seppl".to_string())]));
+        handler.handle_node(&Node::new(3, 1, Coordinate::new(1.0f64, 1.0f64), 1, 1, 1, "a".to_string(), true, vec![Tag::new("bla".to_string(), "hotzenplotz".to_string())]));
+        handler.handle_node(&Node::new(4, 1, Coordinate::new(1.0f64, 1.0f64), 1, 1, 1, "a".to_string(), true, vec![Tag::new("bla".to_string(), "großmutter".to_string())]));
         let result = handler.get_result(HandlerResult::new());
         assert_eq!(result.count_all_nodes, 4);
         assert_eq!(result.count_accepted_nodes, 2);
+
     }
 }
