64b4bb999956aba4e10a4560772616978ad03c7a
Jochen Haeussler <jochen.haeussler@heigit.org>
2024-07-05 17:04:26 +0200

    fix: fix config for test

--- src/conf.rs
+++ src/conf.rs
@@ -10,7 +10,7 @@ impl Config {
     pub fn default() -> Self {
         Self {
             param: 0,
-            country_path: "./test/borders_debenelux.csv".to_string(),
+            country_path: "test/mapping_test.csv".to_string(),
             input_path:  "test/baarle_small.pbf".to_string(),
             output_path:  "output.pbf".to_string(),
         }
