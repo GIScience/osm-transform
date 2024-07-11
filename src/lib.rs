ef0e9ff7b0745a8401b03758d94e97b8eb4dd835
Jochen Haeussler <jochen.haeussler@heigit.org>
2024-07-11 14:26:59 +0200

    chore: cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -37,35 +37,6 @@ pub fn run(config: &Config) {
     let mut handler_chain = HandlerChain::default().add_unboxed(area_handler).add_unboxed(output_handler);
     let _ = process_with_handler(config, &mut handler_chain).expect("Area handler failed");
     log::info!("Finished mapping, time: {}", stopwatch);
-
-    // process_file().expect("did not work");
-
-    // read pbf, filter node ids belonging to ways -> node_ids, extract bbox, maxId (gefilterte)
-    // reader(config, filter, bbox_extracotr, max_id_extractor);
-
-    // let mut bbox_collector = BboxCollector{next: None, min_lat: 0f64, min_lon: 0f64, max_lat: 0f64, max_lon: 0f64};
-    // let mut filter = Filter{next: &bbox_collector, node_ids: Vec::new(), way_ids: Vec::new()};
-    // process_with_handler(config, filter);
-
-    // download geotiffs for bbox
-    // geo_tiff_downloader(config, bbox_extractor);
-
-    // read pbf, nodes: handle notes to keep
-    //                      remove tags
-    //                      if elevation: add ele tag
-    //                      if interpolation & elevation: add node_id:coordinates
-    //                      if country: add country tag
-    //                      write node to node pbf file nodes1
-    // reader(config, filter, remove_tags, elevation_handler, interpolation_handler, country_handler, output_handler)
-
-    //            ways:
-    //                    remove tags
-    //                    if interpolation: interpolate: create new nodes an add to nodes1
-    //                    write way to ways
-    //             relations:
-    //                  remove tags
-    //                  write
-    //  if interpolated : merge files
 }
 
 #[cfg(test)]
