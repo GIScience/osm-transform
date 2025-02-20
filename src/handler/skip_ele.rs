18e2126360bfcf22abf69857eb76ae2807ecf514
Jochen Haeussler <jochen.haeussler@heigit.org>
2025-02-20 18:42:51 +0100

    adapt SkipElevationNodeCollector to new trait fn

--- src/handler/skip_ele.rs
+++ src/handler/skip_ele.rs
@@ -4,7 +4,6 @@ use osm_io::osm::model::way::Way;
 use crate::handler::{HandlerResult, HIGHEST_NODE_ID, Handler};
 
 pub(crate) struct SkipElevationNodeCollector {
-    skip_elevation_node_ids: BitVec,
     no_elevation_keys: Vec<String>,
 }
 impl SkipElevationNodeCollector {
@@ -12,7 +11,6 @@ impl SkipElevationNodeCollector {
 
     pub(crate) fn new(nbits: usize, no_elevation_keys: Vec<&str>) -> Self {
         Self {
-            skip_elevation_node_ids: BitVec::from_elem(nbits, false),
             no_elevation_keys: no_elevation_keys.iter().map(|&str| String::from(str)).collect()
         }
     }
@@ -25,34 +23,29 @@ impl SkipElevationNodeCollector {
         way.tags().iter().any(|tag| self.no_elevation_keys.contains(&tag.k()) && tag.v() != "no")
     }
 
-    fn handle_way(&mut self, way: &Way) {
-        if self.skip_elevation(way) {
-            log::trace!("skipping elevation for way {}", way.id());
-            for &id in way.refs() {
-                log::trace!("skipping elevation for node {}", id);
-                self.skip_elevation_node_ids.set(id as usize, true);
-            }
-        }
-    }
 }
 impl Handler for SkipElevationNodeCollector {
     fn name(&self) -> String {
         String::from("SkipElevationNodeCollector")
     }
 
-    fn handle_ways(&mut self, elements: Vec<Way>) -> Vec<Way> {
-        elements.iter().for_each(|way| self.handle_way(way));
-        elements
-    }
-
-    fn add_result(&mut self, result: &mut HandlerResult) {
-        result.skip_ele = self.skip_elevation_node_ids.clone();
+    fn handle_result(&mut self, result: &mut HandlerResult) {
+        for way in & result.ways {
+            if self.skip_elevation(way) {
+                log::trace!("skipping elevation for way {}", way.id());
+                for &id in way.refs() {
+                    log::trace!("skipping elevation for node {}", id);
+                    result.skip_ele.set(id as usize, true);
+                }
+            }
+        }
     }
 }
 
 
 #[cfg(test)]
 mod test {
+    use crate::handler::{Handler, HandlerResult};
     use crate::handler::skip_ele::SkipElevationNodeCollector;
     use crate::handler::tests::{simple_way};
     const TUNNEL: (&str, &str) = ("tunnel", "avalanche_protector");
@@ -75,35 +68,36 @@ mod test {
     }
 
     #[test]
-    fn test_handle_way() {
-        let mut collector = SkipElevationNodeCollector::new(10, SkipElevationNodeCollector::DEFAULT_KEYS.to_vec());
-        let ways = vec![
-            simple_way(1, vec![1, 2, 3], vec![HIGHWAY]),
-            simple_way(2, vec![3, 4], vec![HIGHWAY, BRIDGE]),
-            simple_way(3, vec![4, 5, 6], vec![HIGHWAY, NO_BRIDGE]),
-            simple_way(4, vec![6, 7], vec![HIGHWAY, NO_BRIDGE, CUTTING]),
-            simple_way(5, vec![7, 8, 9], vec![HIGHWAY, TUNNEL])
-        ];
-        for way in ways {
-            collector.handle_way(&way);
-        }
+    fn test_skip_elevation_node_collector_handle_result() {
+        let mut result = HandlerResult::default();
+        let mut collector = SkipElevationNodeCollector::default();
+        result.ways.push(simple_way(1, vec![1, 2, 3], vec![HIGHWAY]));
+        result.ways.push(simple_way(2, vec![3, 4], vec![HIGHWAY, BRIDGE]));
+        result.ways.push(simple_way(3, vec![4, 5, 6], vec![HIGHWAY, NO_BRIDGE]));
+        result.ways.push(simple_way(4, vec![6, 7], vec![HIGHWAY, NO_BRIDGE, CUTTING]));
+        result.ways.push(simple_way(5, vec![7, 8, 9], vec![HIGHWAY, TUNNEL]));
 
-        assert!(!collector.skip_elevation_node_ids.get(0).unwrap_or(false) );
-        assert!(!collector.skip_elevation_node_ids.get(1).unwrap_or(false) );
-        assert!(!collector.skip_elevation_node_ids.get(2).unwrap_or(false) );
-        assert!( collector.skip_elevation_node_ids.get(3).unwrap_or(false) );
-        assert!( collector.skip_elevation_node_ids.get(4).unwrap_or(false) );
-        assert!(!collector.skip_elevation_node_ids.get(5).unwrap_or(false) );
-        assert!( collector.skip_elevation_node_ids.get(6).unwrap_or(false) );
-        assert!( collector.skip_elevation_node_ids.get(7).unwrap_or(false) );
-        assert!( collector.skip_elevation_node_ids.get(8).unwrap_or(false) );
-        assert!( collector.skip_elevation_node_ids.get(9).unwrap_or(false) );
+        collector.handle_result(&mut result);
+
+        assert!(!result.skip_ele.get(0).unwrap_or(false) );
+        assert!(!result.skip_ele.get(1).unwrap_or(false) );
+        assert!(!result.skip_ele.get(2).unwrap_or(false) );
+        assert!( result.skip_ele.get(3).unwrap_or(false) );
+        assert!( result.skip_ele.get(4).unwrap_or(false) );
+        assert!(!result.skip_ele.get(5).unwrap_or(false) );
+        assert!( result.skip_ele.get(6).unwrap_or(false) );
+        assert!( result.skip_ele.get(7).unwrap_or(false) );
+        assert!( result.skip_ele.get(8).unwrap_or(false) );
+        assert!( result.skip_ele.get(9).unwrap_or(false) );
     }
 
+    #[ignore]//SkipElevationNodeCollector uses now the bitvec of HandlerResult which is initialized with full size
     #[test]
     #[should_panic(expected = "index out of bounds")]
     fn node_id_collector_out_of_bounds(){
         let mut collector = SkipElevationNodeCollector::new(10, SkipElevationNodeCollector::DEFAULT_KEYS.to_vec());
-        collector.handle_way( &simple_way(1, vec![9, 10], vec![TUNNEL]) );
+        let mut result = HandlerResult::default();
+        result.ways.push(simple_way(1, vec![9, 10], vec![TUNNEL]));
+        collector.handle_result(&mut HandlerResult::default());
     }
 }
